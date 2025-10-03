8832e71137c1e2b6bb64cc150e188fc66b1335a5
Ed Pizzella <ed.pizzella@gmail.com>
2025-10-02 22:22:54 -0400

    Fixed asteroid collision

--- astroid.gd
+++ astroid.gd
@@ -5,10 +5,26 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var x := 0.0
+	var y := 0.0
+
+	if position.x >= screen_size.x:
+		x = -1 * screen_size.x
+	elif position.x <= 0:
+		x = screen_size.x
+
+	if position.y >= screen_size.y:
+		y = -1 * screen_size.y
+	elif position.y <= 0:
+		y = screen_size.y
+
+	state.transform = state.transform.translated(Vector2(x, y))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
 	# screen wrap
-	position.x = wrapf(position.x, 0, screen_size.x)
-	position.y = wrapf(position.y, 0, screen_size.y)
+	#position.x = wrapf(position.x, 0, screen_size.x)
+	#position.y = wrapf(position.y, 0, screen_size.y)
